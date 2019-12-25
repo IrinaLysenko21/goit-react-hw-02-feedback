b3b66737f5907e2a542b12914bd5358d8393bf58
Irina Lysenko <irina.lysenko21@gmail.com>
2019-12-25 12:33:00 +0200

    code fixes

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -6,22 +6,27 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <ul className={styles.optionsList}>
       {Object.keys(options).map(option => (
-        <button
-          key={option}
-          name={option}
-          className={styles.btn}
-          type="button"
-          onClick={onLeaveFeedback}
-        >
-          {option}
-        </button>
+        <li key={option} className={styles.optionsListItem}>
+          <button
+            name={option}
+            className={styles.optionBtn}
+            type="button"
+            onClick={onLeaveFeedback}
+          >
+            {option}
+          </button>
+        </li>
       ))}
     </ul>
   );
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.shape({}).isRequired,
+  options: PropTypes.shape({
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+  }).isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
 
